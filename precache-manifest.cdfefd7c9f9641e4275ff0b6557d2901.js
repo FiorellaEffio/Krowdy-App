c77e7c101cacb8a435af648386d88897ab547a49
Fiorella Effio <fiorella.effio@unmsm.edu.pe>
2019-03-06 10:03:58 -0500

    Updates

---
+++ precache-manifest.cdfefd7c9f9641e4275ff0b6557d2901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Krowdy-App/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0d63b2681c54e3a93a5e",
+    "url": "/Krowdy-App/static/js/runtime~main.0d63b268.js"
+  },
+  {
+    "revision": "5078043f597e8a79be07",
+    "url": "/Krowdy-App/static/js/main.5078043f.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/Krowdy-App/static/js/2.a539ea9d.chunk.js"
+  },
+  {
+    "revision": "5078043f597e8a79be07",
+    "url": "/Krowdy-App/static/css/main.677c97e9.chunk.css"
+  },
+  {
+    "revision": "89d983457d96334c76647865e4cdf96a",
+    "url": "/Krowdy-App/index.html"
+  }
+];
